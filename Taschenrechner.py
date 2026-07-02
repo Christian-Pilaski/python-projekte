7cff87785da19a2c73a16b4100a57a82fe230f53
Christian Pilaski <christian.pilaski@gmail.com>
2026-07-02 18:34:44 +0200

    Taschenrechner mit standart arithmetischer Logik

---
+++ Taschenrechner.py
@@ -0,0 +1,72 @@
+running = True
+
+    
+def addition(a,b):
+    """
+    Addiert Zahlen 
+    Args:
+        a: Erste Zahl
+        b: Zweite Zahl
+    Return c (Ergebnis)
+    """
+    c = a+b
+    return c
+   
+def subtraktion(a,b):
+    """
+    Subtrahiert Zahlen 
+    Args:
+        a: Erste Zahl
+        b: Zweite Zahl
+    Return c (Ergebnis)
+    """
+    c = a-b
+    return c
+        
+def multiplikation(a,b):
+    """
+    Multipliziert Zahlen 
+    Args:
+        a: Erste Zahl
+        b: Zweite Zahl
+    Return c (Ergebnis)
+    """
+    c = a*b
+    return c
+
+def division(a,b):
+    """
+    Dividiert Zahlen und fängt teilen durch 0 ab
+    Args:
+        a: Erste Zahl
+        b: Zweite Zahl
+    Return: Ergebnis der Berechnung als float, oder None bei Division durch 0
+    """
+    try:
+        c= a/b  
+    except ZeroDivisionError:
+        print("Ein Fehler ist Aufgetreten: Division durch 0")  
+        c= None
+    return c
+        
+
+
+if  __name__== "__main__": 
+    while running:
+        eins = float(input("1.Zahl: "))
+        operator = input("Bitte wähle deine Rechenart(+-*/): ")
+        zwei = float(input("2.Zahl: "))
+        
+        if operator == "+":
+            print("Addition: ", addition(eins,zwei))
+        elif operator == "-":
+            print("Subtraktion: ", subtraktion(eins,zwei))
+        elif operator == "*":
+            print("Multiplikation: ", multiplikation(eins,zwei))
+        elif operator == "/":
+            print("Division: ", division(eins,zwei))
+        else:
+            print("Ungültige eingabe!")
+        x = input("Abbrechen ? Press y:  ").lower()
+        if x == "y":
+            running = False
